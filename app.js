ecb3730392d5c5529298caa519f085c8ef337b97
Bob Hiltner <bobhilt@gmail.com>
2019-01-22 06:16:48 -1000

    Add compose, post view, bootstrap, and basic styling.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
+const path = require('path');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,11 +16,11 @@ const posts = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //get
 app.get("/", (req, res) => {
-  res.render('home', {Content: posts});
+  res.render('home', {posts: posts});
 });
 
 app.get("/about", (req, res) => {
@@ -33,6 +35,29 @@ app.get("/compose", (req, res) => {
   res.render('compose');
 });
 
+app.get("/posts/:post", (req, res) => {
+  let match = false;
+  const post = _.toLower(req.params.post);
+  let postToRender;
+
+  for( let i=0; i<posts.length; i++){
+    const title = _.toLower(posts[i].title);
+
+    if (title === post) {
+      match = true;
+      postToRender = posts[i] ;      
+      break;
+    }
+  }
+  if (match) {
+    console.log(postToRender);
+    res.render('post', {content: postToRender} );
+  } else {
+    res.status(404).send('Sorry, we cannot find that!');
+  }
+});
+
+
 
 //post
 app.post("/compose", (req, res) => {
@@ -40,7 +65,7 @@ app.post("/compose", (req, res) => {
     title: req.body.postTitle,
     content: req.body.postContent
   }
-  console.log(post);
+  //console.log(post);
   posts.push(post);
   console.log(posts);
   
